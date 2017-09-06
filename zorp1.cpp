50abe265bbf03446e4ace43a47db7e20b6e2adaa
RalenskiDoucet <Ralenskid@gmail.com>
2017-09-06 09:54:01 -0500

    commit

---
+++ zorp1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+void main()
+{
+	std::cout << "HelloWord";
+	int height = 0;
+	std::cout << "welcome to ZORP!";
+	std::cout << "ZORP is a game of adventure,danger, and low cunning.";
+	std::cout<< "It's defintly fun.";
+
+	system("pause");
+	return;
+}
